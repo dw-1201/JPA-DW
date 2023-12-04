7f88d3f39d53436edfaf67807fb0a0870ec9e8a6
HyeongJunIm <chaesng6064@gmail.com>
2023-12-04 15:28:53 +0900

    no message

---
+++ src/main/java/com/example/dw/controller/QnaController.java
@@ -0,0 +1,20 @@
+package com.example.dw.controller;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/qna/*")
+public class QnaController {
+
+    @GetMapping("/qmain")
+    public String main(){
+        return "/community/qnaList";
+    }
+
+}
