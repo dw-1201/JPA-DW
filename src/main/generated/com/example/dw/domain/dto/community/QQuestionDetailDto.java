a0e760d557798616cd500e78ff324fd57fc003b3
HyeongJunIm <chaesng6064@gmail.com>
2023-12-18 16:16:41 +0900

    수정중

---
+++ src/main/generated/com/example/dw/domain/dto/community/QQuestionDetailDto.java
@@ -0,0 +1,21 @@
+package com.example.dw.domain.dto.community;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.ConstructorExpression;
+import javax.annotation.processing.Generated;
+
+/**
+ * com.example.dw.domain.dto.community.QQuestionDetailDto is a Querydsl Projection type for QuestionDetailDto
+ */
+@Generated("com.querydsl.codegen.DefaultProjectionSerializer")
+public class QQuestionDetailDto extends ConstructorExpression<QuestionDetailDto> {
+
+    private static final long serialVersionUID = -1031332364L;
+
+    public QQuestionDetailDto(com.querydsl.core.types.Expression<Long> id, com.querydsl.core.types.Expression<String> questionTitle, com.querydsl.core.types.Expression<String> questionContent, com.querydsl.core.types.Expression<String> questionRd, com.querydsl.core.types.Expression<Long> questionImgId, com.querydsl.core.types.Expression<String> questionImgRoute, com.querydsl.core.types.Expression<String> questionImgUuid, com.querydsl.core.types.Expression<String> questionImgName, com.querydsl.core.types.Expression<Long> userId, com.querydsl.core.types.Expression<String> userName) {
+        super(QuestionDetailDto.class, new Class<?>[]{long.class, String.class, String.class, String.class, long.class, String.class, String.class, String.class, long.class, String.class}, id, questionTitle, questionContent, questionRd, questionImgId, questionImgRoute, questionImgUuid, questionImgName, userId, userName);
+    }
+
+}
+
--- src/main/java/com/example/dw/service/QnaService.java
+++ src/main/java/com/example/dw/service/QnaService.java
@@ -10,21 +10,25 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class QnaService {
 
+    private final FileService fileService;
     private final QuestionRepository questionRepository;
     private final UsersRepository usersRepository;
     // 글 작성, 사진 파일 저장
     @Transactional
     public Long register(QuestionWritingForm questionWritingForm) throws IOException {
         System.out.println(questionWritingForm.toString()+"WW");
+        System.out.println(questionWritingForm.toEntity()+"11");
         Question question = questionRepository.save(questionWritingForm.toEntity());
 
 
@@ -42,5 +46,25 @@ public class QnaService {
 
         return questionList;
     }
+
+    //작성 글 수정
+//    @Transactional
+//    public Question modify(QuestionWritingForm questionWritingForm, List<MultipartFile> files)
+//    throws IOException{
+//
+//        if(!files.get(0).isEmpty()){
+//            fileService.removequeDetailImgs(questionWritingForm.getId());
+//
+//            fileService.registerquestionImg(files,questionWritingForm.getId());
+//        }
+//
+//        Question question = questionRepository.findById(questionWritingForm.getId()).get();
+//
+//        //qna 기본내용 업데이트
+//        question.update(questionWritingForm);
+//        System.out.println("서비스단 완료");
+//        return Optional.ofNullable(question).orElseThrow(()->{
+//            throw new IllegalArgumentException("조회 정보 없음");});
+//    }
 
 }
