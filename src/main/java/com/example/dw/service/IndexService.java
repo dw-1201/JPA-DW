094bded96093a1e7d8fa0d7f73c09022caf13a27
bokkaa <bokyh0327@gmail.com>
2024-01-08 18:49:19 +0900

    메인페이지 수정 - 최근 본 상품 추가.

--- src/main/java/com/example/dw/service/IndexService.java
+++ src/main/java/com/example/dw/service/IndexService.java
@@ -1,7 +1,10 @@
 package com.example.dw.service;
 
 import com.example.dw.domain.dto.goods.IndexGoodsByCateDto;
+import com.example.dw.domain.dto.goods.RecentViewGoodsDto;
 import com.example.dw.repository.goods.GoodsRepositoryCustom;
+import com.example.dw.repository.goods.ShopRepositoryCustom;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +17,25 @@ import java.util.List;
 public class IndexService {
 
     private final GoodsRepositoryCustom goodsRepositoryCustom;
-
+    private final ShopRepositoryCustom shopRepositoryCustom;
 
     @Transactional
     public List<IndexGoodsByCateDto> indexGoodsByCategory(String cate){
 
         return goodsRepositoryCustom.indexGoodsListByCategory(cate);
+    }
+
+    //최근 본 상품
+    @Transactional
+    public List<RecentViewGoodsDto> recentViewGoods(HttpSession session){
+
+
+        try{
+            return shopRepositoryCustom.recentViewGoods(session);
+
+        }catch (NullPointerException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
