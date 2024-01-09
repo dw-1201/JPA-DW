7af19faddb1205254f26c69771f852c3d1dece78
bokkaa <bokyh0327@gmail.com>
2024-01-09 17:52:55 +0900

    산책글 작성 제한

--- src/main/java/com/example/dw/controller/IndexController.java
+++ src/main/java/com/example/dw/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.dw.controller;
 
+import com.example.dw.domain.dto.community.IndexWalkMateDto;
 import com.example.dw.domain.dto.goods.RecentViewGoodsDto;
 import com.example.dw.service.IndexService;
 import jakarta.servlet.http.HttpSession;
@@ -19,15 +20,26 @@ public class IndexController {
 
     private final IndexService indexService;
 
+    
+    
+    //Index
     @GetMapping("")
     public String index(HttpSession session, Model model){
 
+        //산책글 리스트
+
+        List<IndexWalkMateDto> indexWalkMateDtoList = indexService.indexWalkMateList();
+
+        model.addAttribute("indexWalkList", indexWalkMateDtoList);
+        System.out.println(indexWalkMateDtoList.toString()+"!@#@!#@!");
+
+        
+        
+        //최근 본 상품
         List<RecentViewGoodsDto> recentGoods = indexService.recentViewGoods(session);
 
         model.addAttribute("recentGoods", recentGoods);
-
-        System.out.println(recentGoods+"!@#@!#@!#!@#@!#");
-
+        
 
         return "/index/index";
     }
